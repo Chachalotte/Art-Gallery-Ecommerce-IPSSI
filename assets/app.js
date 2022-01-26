aec9300dd350decedc513e5d64a609cc41b1b042
Anthony <anthony.stanix@gmail.com>
2022-01-26 15:19:05 +0100

    add sass files

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,6 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-
+import './sass/app.scss';
 // start the Stimulus application
 import './bootstrap';
