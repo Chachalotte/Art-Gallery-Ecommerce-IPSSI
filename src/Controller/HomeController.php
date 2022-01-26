de3f6c9ce9af748edf02b77c3b15bcbb77d4d3df
Anthony <anthony.stanix@gmail.com>
2022-01-26 22:19:03 +0100

    add entities on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Artist;
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $artists = $doctrine->getRepository(Artist::class)->findAll();
+        $products = $doctrine->getRepository(Product::class)->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'artists' => $artists,
+            'products' => $products,
         ]);
     }
 }
