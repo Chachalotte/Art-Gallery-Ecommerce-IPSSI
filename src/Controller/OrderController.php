f87d8de1fd267fca2084197bbcb08cffdc576637
Anthony Stanix <anthony.stanix@gmail.com>
2022-07-16 18:45:43 +0200

    add order

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderController extends AbstractController
+{
+    #[Route('/commande', name: 'order')]
+    public function index(): Response
+    {
+        return $this->render('order/index.html.twig');
+    }
+}
