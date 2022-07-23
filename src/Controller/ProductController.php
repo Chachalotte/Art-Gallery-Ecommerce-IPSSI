11f85a1c20c3a73ce3d9fb1d19d26724aed9e603
IsianDreemrr <florian.alvarez.rodriguez@gmail.com>
2022-07-23 10:13:44 +0200

    Changing forms + their css

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -65,7 +65,7 @@ class ProductController extends AbstractController
             );
             $product->setImg($imageName);
             $product->setArtist($user);
-            $product->setOrderItem(NULL);
+            //$product->setOrderItem(NULL);
 
             // On le persist et l'enregistre en BDD
             $em->persist($product);
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
@@ -17,12 +18,19 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('name')
-            ->add('firstname')
+            ->add('email', TextType::class,[
+                'label' => 'Adresse email'
+            ])
+            ->add('name', TextType::class,[
+                'label' => 'Nom'
+            ])
+            ->add('firstname', TextType::class,[
+                'label' => 'Prénom'
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => 'Mot de passe',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
