929e9d1520ddbeb72ba9623edaa297f613dbff0d
Anthony Stanix <anthony.stanix@gmail.com>
2022-08-10 22:24:09 +0200

    create the dashboard frame

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,25 +5,79 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use App\Entity\Order;
 use App\Entity\Carrier;
+use App\Entity\Contact;
 use App\Entity\Product;
 use App\Entity\Category;
-use App\Entity\Contact;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
-use Symfony\UX\Chartjs\Model\Chart;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function configureAssets(): Assets
+    {
+        return Assets::new()->addCssFile('css/dashboard.css');
+    }
+    
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index():Response
     {
+        $dateNow = new \DateTime();
+        $dateStart = new \DateTime();
+        $dateStart->setTimestamp($dateNow->getTimestamp() - 2592000);
+
+        $products = $this->doctrine->getRepository(Product::class)->findAll();
+        $users = $this->doctrine->getRepository(User::class)->findAll();
+
+        $allArtistArray = array();
+
+        foreach ($users as $userInst) {
+            $userId = $userInst->getId(); 
+            $userRoles = $userInst->getRoles(); 
+
+            if(in_array('ROLE_ARTIST', $userRoles))
+            {
+                $allArtistArray[$userId] = $userInst;
+            }
+        }
+
+        $totalAll = 0;
+        $ordersAll = $this->doctrine->getRepository(Order::class)->findPaidOrders();
+        foreach($ordersAll as $o){  
+            $od = $o->getOrderDetails();  
+            foreach($od as $detail){
+                $totalAll += $detail->getTotal();
+            }                    
+        }
+
+        $totalThirty = 0;
+        $ordersThirty = $this->doctrine->getRepository(Order::class)->findByThirtyDays($dateStart, $dateNow);
+        foreach($ordersThirty as $o){  
+            $od = $o->getOrderDetails();  
+            foreach($od as $detail){
+                $totalThirty += $detail->getTotal();
+            }                    
+        }
+
         // $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
         // return $this->render('@EasyAdmin/page/content.html.twig');
-        return $this->render('admin/dashboard.html.twig', []);
+        return $this->render('admin/dashboard.html.twig', [
+            'orders' => $ordersAll,
+            'total' => $totalAll,
+            'orders30' => $ordersThirty,
+            'total30' => $totalThirty,
+            'products' => $products,
+            'artists' => $allArtistArray
+        ]);
     }
 
     public function configureDashboard(): Dashboard
